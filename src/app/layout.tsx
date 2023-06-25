84162441f62a9332969b66444859b72b80504d4b
Michał Bryk <galeriakoloru90210@gmail.com>
2023-06-25 12:34:36 +0200

    implement toaster library

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 
 import Navbar from "@/components/Navbar";
+import { Toaster } from "@/components/ui/Toaster";
 
 export const metadata = {
   title: "Breadit",
@@ -18,6 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className="min-h-screen pt-12 bg-slate-50 antialiased">
         <Navbar />
         <div className="container max-w-7xl mx-auto h-full pt-12">{children}</div>
+        <Toaster />
       </body>
     </html>
   );
--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -1,17 +1,18 @@
 "use client";
 
-import { FC, useState } from "react";
-import { signIn } from "next-auth/react";
-
 import { cn } from "@/lib/utils";
-
-import { Button } from "./ui/Button";
+import { signIn } from "next-auth/react";
+import * as React from "react";
+import { FC } from "react";
+import { Button } from "@/components/ui/Button";
+import { useToast } from "@/hooks/use-toast";
 import { Icons } from "./Icons";
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { toast } = useToast();
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
 
   const loginWithGoogle = async () => {
     setIsLoading(true);
@@ -19,7 +20,11 @@ const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
     try {
       await signIn("google");
     } catch (error) {
-      //TODO: toast notifiaction
+      toast({
+        title: "Error",
+        description: "There was an error logging in with Google",
+        variant: "destructive",
+      });
     } finally {
       setIsLoading(false);
     }
@@ -27,7 +32,14 @@ const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
 
   return (
     <div className={cn("flex justify-center", className)} {...props}>
-      <Button onClick={loginWithGoogle} isLoading={isLoading} size="sm" className="w-full">
+      <Button
+        isLoading={isLoading}
+        type="button"
+        size="sm"
+        className="w-full"
+        onClick={loginWithGoogle}
+        disabled={isLoading}
+      >
         {isLoading ? null : <Icons.google className="h-4 w-4 mr-2" />}
         Google
       </Button>
