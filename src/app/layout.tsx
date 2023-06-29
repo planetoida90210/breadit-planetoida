6a47bff3823c633d09d9e0fac32bd0802c747e80
Michał Bryk <galeriakoloru90210@gmail.com>
2023-06-29 12:14:16 +0200

    tanstack query provider setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 
 import Navbar from "@/components/Navbar";
-import { Toaster } from "@/components/ui/Toaster";
+import Providers from "@/components/Providers";
 
 export const metadata = {
   title: "Breadit",
@@ -23,11 +23,12 @@ export default function RootLayout({
   return (
     <html lang="en" className={cn("bg-white text-slate-900 antialiased light", inter.className)}>
       <body className="min-h-screen pt-12 bg-slate-50 antialiased">
-        {/* @ts-expect-error server component */}
-        <Navbar />
-        {authModal}
-        <div className="container max-w-7xl mx-auto h-full pt-12">{children}</div>
-        <Toaster />
+        <Providers>
+          {/* @ts-expect-error server component */}
+          <Navbar />
+          {authModal}
+          <div className="container max-w-7xl mx-auto h-full pt-12">{children}</div>
+        </Providers>
       </body>
     </html>
   );
